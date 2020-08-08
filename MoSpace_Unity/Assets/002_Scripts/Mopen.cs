3accbead7abf67ee94fb46a10fb2b19790a1b0ca
AwuChen_PC <szeyuche@usc.edu>
2020-08-08 14:47:46 -0700

    bringing unity project and testing new build

---
+++ MoSpace_Unity/Assets/002_Scripts/Mopen.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Mopen : MonoBehaviour
+{
+    string userName;
+    bool runOnce = false;
+    public bool pri = false;
+    public bool pub = true;
+    public bool per = false;
+
+    public GameObject[] privateSpace;
+    public GameObject[] publicSpace;
+    public GameObject[] personalSpace;
+    void Start()
+    {
+        
+    }
+
+    private void CheckAccess()
+    {
+
+        print("checkAccess");
+        //only active for awu & friends 
+        if (userName == "Awu" || userName == "awu" || userName == "friend")
+        {
+            print("checkAccess2");
+            for (int i = 0; i < personalSpace.Length; i++)
+            {
+                personalSpace[i].SetActive(true);
+                print("checkAccess3");
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // get local player name and check access 
+        if ( !runOnce && other.tag == "LocalPlayer")
+        {
+            userName = other.gameObject.GetComponentInChildren<TextMesh>().text;
+            print(userName);
+            CheckAccess();
+            runOnce = true;
+        }
+        //private
+        if (pri)
+        {
+            if (other.tag == "NetworkPlayer")
+            {
+                other.gameObject.GetComponent<PlayerManager>().HidePlayer(true);
+            }
+        }
+        //personal
+        if (per)
+        {
+            //not awu but see awu and awu friend 
+            if (userName != "Awu" && userName != "awu" && userName != "friend")
+            {
+                other.gameObject.GetComponent<PlayerManager>().HidePlayer(true);
+            }
+            else
+            {
+                other.gameObject.GetComponent<PlayerManager>().HidePlayer(false);
+            }
+        }
+        //public
+        if (pub)
+        {
+            if (other.tag == "NetworkPlayer")
+            {
+                other.gameObject.GetComponent<PlayerManager>().HidePlayer(false);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+        //private 
+        if (pri)
+        {
+            if (other.tag == "NetworkPlayer")
+            {
+                other.gameObject.GetComponent<PlayerManager>().HidePlayer(false);
+            }
+        }
+        //personal
+        if (per)
+        {
+            //not awu but see awu and awu friend 
+            if (userName != "Awu" && userName != "awu" && userName != "friend")
+            {
+                other.gameObject.GetComponent<PlayerManager>().HidePlayer(false);
+            }
+        }
+
+    }
+
+    IEnumerator Wait()
+    {
+        //yield on a new YieldInstruction that waits for 3 seconds.
+        yield return new WaitForSeconds(3);
+
+        Application.OpenURL("https://shimo.im/boards/Vv7dJxrHziYmYUVC/");
+        runOnce = true;
+    }
+
+}
+
+
+
