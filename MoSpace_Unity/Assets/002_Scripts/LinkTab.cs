3d0de7d70c8d7cc811cc9ff36303dd17b6ae5a6a
AwuChen_PC <szeyuche@usc.edu>
2020-09-23 23:34:58 -0700

    yc

---
+++ MoSpace_Unity/Assets/002_Scripts/LinkTab.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class LinkTab : MonoBehaviour 
+{
+
+	public InputField Field;
+    public string link;
+    public void OpenLink()
+	{
+		Application.OpenURL(Field.text);
+	}
+
+	public void OpenLinkJS()
+	{
+		Application.ExternalEval("window.open('"+link+"');");
+	}
+
+	public void OpenLinkJSPlugin()
+	{
+		#if !UNITY_EDITOR
+		openWindow(Field.text);
+		#endif
+	}
+
+	[DllImport("__Internal")]
+	private static extern void openWindow(string url);
+
+}
