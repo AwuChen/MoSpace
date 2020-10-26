5247aced808cf4fa5c1dbb41f2a8615f0c78579f
AwuChen_PC <szeyuche@usc.edu>
2020-10-25 18:39:57 -0700

    sexy app first push

---
+++ MoSpace_Unity/Assets/002_Scripts/TextCommand.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using UnityEngine.UI;
+
+public class TextCommand : MonoBehaviour
+{
+    public GameObject[] spaces;
+    public Text UserInput;
+    // Update is called once per frame
+    
+    public void SubmitText()
+    {
+        for (int i = 0; i < spaces.Length; i++)
+        {
+            if ((UserInput.text.ToString()).ToLower() == spaces[i].name.ToLower())
+            {
+                
+                if (spaces[i].activeSelf)
+                {
+                    spaces[i].SetActive(false);
+                }else
+                {
+                    spaces[i].SetActive(true);
+                }
+            }
+        }
+    }
+}
