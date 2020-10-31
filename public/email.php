01bacfe4569ef2d4f38c661ec875718a9dce3690
AwuChen_PC <szeyuche@usc.edu>
2020-10-31 00:41:36 -0700

    added composer

--- public/email.php
+++ public/email.php
@@ -7,5 +7,11 @@
 Welcome <br>
 Your email address is: <?php echo $_GET["email"]; ?>
 
+<?php
+  echo 'This is Index Page';
+  $sql = 'INSERT INTO users(name) VALUES (\'tesname4\');';
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute();
+?>
 </body>
 </html>
