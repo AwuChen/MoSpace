370490cf41ee07afd7ba16495d9fc94e9bf2a4df
AwuChen_Blade <szeyuche@usc.edu>
2020-11-17 17:31:12 -0800

    scrolling refined, UI polished

--- MoSpace_Unity/Assets/WebGL Multiplayer Online Client/BasicExample/Scripts/Network/NetworkManager.cs
+++ MoSpace_Unity/Assets/WebGL Multiplayer Online Client/BasicExample/Scripts/Network/NetworkManager.cs
@@ -93,14 +93,7 @@ public class NetworkManager : MonoBehaviour {
 
 	}
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log("space");
-            OnUpdateWriting("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum");
-        }
-    }
+   
     // Use this for initialization
     void Start () {
 
